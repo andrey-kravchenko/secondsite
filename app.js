ab5a38daa7258f4bc9a093ca00d31b777ce73700
andrey-kravchenko <akrava1982@gmail.com>
2020-10-05 21:49:11 +0300

    добавил изменения

--- app.js
+++ app.js
@@ -11,4 +11,14 @@ $(function() {
 
     });
 
+    $("[data-modal]").on("click", function(event){
+        event.preventDefault();
+        
+        $(modal_resume).addClass('show');
+
+    });
+
+    $(modal_resume).on("click", function() {
+        $(modal_resume).removeClass('show');
+    });
 });
